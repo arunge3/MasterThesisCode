cb6767e8d8d703f0aefb8343c19c2f97b3f260bd
arunge3 <arunge3@uni-koeln.de>
2024-10-14 17:41:16 +0200

    fix: edit json generator

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 import json
+from datetime import datetime
 
-path_timeline = "D:\Handball\HBL_Events\season_20_21\EventTimeline\sport_events_23400263_timeline.json"
-path_output = "D:\Handball\HBL_Events\season_20_21\EventJson\sport_events_23400263_timeline_reformatted.jsonl"
-# path_video = "D:\Handball\HBL_Videos\season_2021\2020-10-01_754520_tsv_hannover-burgdorf-gwd_minden-720p-3000kbps.mp4"
+# File paths
+path_timeline = "D:\\Handball\\HBL_Events\\season_20_21\\EventTimeline\\sport_events_23400263_timeline.json"
+path_output_jsonl = "D:\\Handball\\HBL_Events\\season_20_21\\EventJson\\sport_events_23400263_timeline_reformatted.jsonl"
 
-# Load the JSON data from the file
+# Frames per second of the video
+fps = 29.97  # frames per second
+
+# Starttime of the video
+video_start_time = datetime.fromisoformat("2020-10-01T17:00:11+00:00")
+
+# Method for converting event time to timeframe
+def convert_event_time_to_timeframe(event_time_str):
+    event_time = datetime.fromisoformat(event_time_str)
+    time_diff_seconds = (event_time - video_start_time).total_seconds()
+    timeframe = round(time_diff_seconds * fps) +56479
+    return str(timeframe)
+
+# Load json data from file
 with open(path_timeline, 'r') as file:
     data = json.load(file)
 
-# Step 2: Extract events
-events = data.get('timeline', {})
+# extract events from data
+events = data.get('timeline', [])
 
-# Step 3: Reformat events
-reformatted_events = []
-for event in events:
-    reformatted_event = {
-        't_start': event.get('match_clock', None),
-        't_end': event.get('match_clock', None),
-        'annotator': '',
-        'labels': {
-            'event_id': event.get('id'),
-            'event_type': event.get('type'),
-            'event_time': event.get('time'),
-            'match_time': event.get('match_time', None),
-            'competitor': event.get('competitor', None),
-            'player_id': event.get('player', {}).get('id') if event.get('player') else None,
-            'player_name': event.get('player', {}).get('name') if event.get('player') else None
-            },
-        }       
-    reformatted_events.append(reformatted_event)
-
-# Step 4: Save the reformatted events to a new JSON file
-with open(path_output, 'w') as file:
-    json.dump(reformatted_events, file, indent=4)
+# Create Jsonl File and write reformatted events
+with open(path_output_jsonl, 'w') as file:
+    for event in events:
+        reformatted_event = {
+            't_start': convert_event_time_to_timeframe(event.get('time', None)),
+            't_end': '-1',
+            'annotator': event.get('type'),
+            'labels': {
+                "formation": "A", 
+                "pass": "D",
+                # 'event_id': event.get('id'),
+                # 'event_type': event.get('type'),
+                # 'event_time': event.get('time'),
+                # 'match_time': event.get('match_time', None),
+                'competitor': event.get('competitor', None),
+                # 'player_id': event.get('player', {}).get('id') if event.get('player') else None,
+                # 'player_name': event.get('player', {}).get('name') if event.get('player') else None
+            }
+        }
+        file.write(json.dumps(reformatted_event) + '\n')
